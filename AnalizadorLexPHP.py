22d112a7afa885fc820b8e394df4ece1af678089
jair-chaguay <jchaguay@espol.edu.ec>
2024-11-10 12:22:52 -0500

    Palabras reservadas

--- AnalizadorLexPHP.py
+++ AnalizadorLexPHP.py
@@ -16,22 +16,40 @@ tokens=(
 )
 
 reserved={
+    #Constantes en tiempo de ejecución
+    '__class__': '__CLASS__',
+    '__dir__': '__DIR__',
+    '__file__': '__FILE__',
+    '__function__': '__FUNCTION__',
+    '__line__': '__LINE__',
+    '__method__': '__METHOD__',
+    '__namespace__': '__NAMESPACE__',
+    '__trait__': '__TRAIT__',
+    #Palabras Reservadas
     'abstract':'ABSTRACT',
     'and': 'AND',
+    'array':'ARRAY',
+    'as':'AS',
     'break': 'BREAK',
     'case': 'CASE',
+    'catch':'CATCH',
     'class': 'CLASS',
     'clone':'CLONE',
     'continue': 'CONTINUE',
+    'const':'CONST',
+    'declare':'DECLARE',
     'default':'DEFAULT',
     'do': 'DO',
     'echo':'ECHO',
     'else': 'ELSE',
     'elseif': 'ELSEIF',
+    'empty':'EMPTY',
+    'endswitch':'ENDSWITCH',
     'extends': 'EXTENDS',
     'false':'FALSE',
-    'final': 'FINAL',
+    'finally': 'FINALLY',
     'for': 'FOR',
+    'foreach':'FOREACH',
     'function':'FUNCTION',
     'global':'GLOBAL',
     'if':'IF',
@@ -39,11 +57,15 @@ reserved={
     'include':'INCLUDE',
     'instanceof':'INSTANCEOF',
     'interface': 'INTERFACE',
+    'isset':'ISSET',
+    'list':'LIST',
     'namespace':'NAMESPACE',
     'new':'NEW',
     'not': 'NOT',
     'or': 'OR',
+    'print':'PRINT',
     'private': 'PRIVATE',
+    'protected':'PROTECTED',
     'public':'PUBLIC',
     'return':'RETURN',
     'static': 'STATIC',
@@ -51,8 +73,8 @@ reserved={
     'this':'THIS',
     'throw':'THROW',
     'true':'TRUE',
+    'try':'TRY',
     'while': 'WHILE',
-
-
 }
 tokens= tokens + tuple(reserved.values())
+
