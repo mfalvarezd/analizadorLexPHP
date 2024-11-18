8389c9d974a6fbd3369fa54f9ddff670428f219c
Moises Alvarez <mfalvare@espol.edu.ec>
2024-11-18 03:03:49 -0500

    Corrigiendo regla gramatica de if,switch, while, for, body,  estructura para instanciar objetos, definicion de clases

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ def p_sentencia(p):
                 | comparacion
                 | estructurasProgram'''
 
-
+def p_sentencias(p):
+    '''sentencias : sentencia
+                  | sentencia sentencias
+                  | empty'''
 #asignacion de variable
 def p_asignacion(p):
     '''asignacion : VARIABLE EQUALS operaArit SEMICOLON'''
@@ -36,22 +39,24 @@ def p_controlStructure(p):
                         | while
                         | switch'''
 #SECCION DEL IF
+## corregi lo que deberia ir despues de cada else simplemente, creo que asi queda bien
 def p_if(p):
-    '''if : statementif ELSE statementif
-        | statementif ELSE LBRACE condition RBRACE
-        | statementif ELSEIF statementif
+    '''if : statementif ELSE body
+        | statementif ELSE if
+        | statementif ELSEIF if
         | statementif'''
 
 def p_statementif(p):
     '''statementif : IF LPAREN p_conditionProdu RPAREN LBRACE body RBRACE'''
-
+##coregio la produccion de condicion porque despues de condicion debi ir un operador logico
 def p_conditionProdu(p):
     '''conditionProdu : condition
-                    | condition opSymbol conditionProdu'''
+                    | condition opLogic conditionProdu'''
 def p_condition(p):
-    '''condition : valor opSymbol valor'''
+    '''condition : valor opSymbol valor
+                 | LPAREN conditionProdu RPAREN'''
 
-def opSymbol(p):
+def p_opSymbol(p):
     '''opSymbol : EQ
                 | NEQ
                 | STRICTEQ
@@ -66,8 +71,8 @@ def p_opLogic(p):
                 | NOT'''
 
 #SECCION DEL FOR
-def p_for(p):
-    '''for : forStatement'''
+def p_for_loop(p):
+    '''for_loop : forStatement'''
 
 def p_forStatement(p):
     '''forStatement : FOR LPAREN forcondition RPAREN LBRACE body RBRACE'''
@@ -76,32 +81,68 @@ def p_forcondition(p):
     '''forcondition : VARIABLE EQUALS INT SEMICOLON VARIABLE opSymbol INT SEMICOLON VARIABLE DOUBLEPLUS
                     | VARIABLE EQUALS INT SEMICOLON VARIABLE opSymbol INT SEMICOLON VARIABLE DOUBLEMINUS'''
 
-
+##moises Alvarez
 def p_while(p):
-    '''while : '''
+    '''while : WHILE LPAREN condition RPAREN LBRACE body RBRACE
+             | WHILE LPAREN condition RPAREN LBRACE RBRACE'''
 
 def p_switch(p):
-    '''switch : '''
+    '''switch : SWITCH LPAREN condition RPAREN LBRACE caseLists RBRACE
+              | SWITCH LPAREN condition RPAREN LBRACE RBRACE'''
+def p_caseLists(p):
+    '''caseLists: cases default
+                | cases
+                | default
+                | empty'''
+def p_cases(p):
+    '''cases : case cases
+             | case'''            
+def p_case(p):
+    '''case: CASE valor COLON body BREAK SEMICOLON'''
+def p_default(p):
+    '''default: DEFAULT COLON body BREAK SEMICOLON'''
 
 def p_dataStructure(p):
     '''dataStructure : array
                     | map'''
 
 def p_funcionDeclarate(p):
-    '''funcionDeclarate : FUNCTION funcionParen brace
-                        | FUNCTION funcionParen DOUBLEDOT dataType brace
-                        | '''
+    '''funcionDeclarate : FUNCTION ID LPAREN parametros RPAREN LBRACE funcionBody RBRACE
+                        | FUNCTION ID LPAREN parametros RPAREN DOUBLEDOT dataType LBRACE funcionBody RBRACE'''
+def p_funcionBody(p):
+    '''funcionBody : body
+                   | RETURN expresion SEMICOLON'''
+                        
+def p_parametros(p):
+    '''parametros: parametro
+                 | parametro COMMA parametros
+                 | empty'''  
+def p_parametro(p):
+    '''parametro: ID
+                | dataType ID
+                | ID EQUALS valor
+                | dataType ID EQUALS valor'''                                   
 def p_arrowfunction(p):
-    '''arrowfunction : FN LPAREN repiteValores RPAREN ARROWMAP operaArit SEMICOLON body'''
+    '''arrowfunction : FN LPAREN parametros RPAREN ARROWMAP expresion SEMICOLON'''
 
 def p_brace(p):
     '''brace : LBRACE body RBRACE'''
 
 def p_funcionParen(p):
-    '''funcionParen : ID LPAREN repiteValores RPAREN'''
-
-def classDeclarate(p):
-    '''classDeclarate : '''
+    '''funcionParen : ID LPAREN parametros RPAREN'''
+#moises Alvarez
+def p_classDeclarate(p):
+    '''classDeclarate : CLASS ID LBRACE classBody RBRACE
+                      | CLASS ID EXTENDS ID LBRACE classBody RBRACE'''
+def p_classBody(p):
+    '''classBody : classMember classBody
+                 | empty'''   
+def p_classMember(p):
+    '''classMember : dataType VARIABLE SEMICOLON
+                   | dataType FUNCTION ID LPAREN parametros RPAREN brace'''                                         
+def p_accessMember(p):
+    '''accessMember : VARIABLE ARROW ID
+                    | VARIABLE ARROW funcionParen'''
 
 def p_array(p):
     '''array : VARIABLE EQUALS LBRACKET repiteValores RBRACKET SEMICOLON
@@ -112,14 +153,16 @@ def p_map(p):
             | VARIABLE EQUALS ARRAY LPAREN mapProduc RPAREN SEMICOLON'''
 
 def p_mapProduc(p):
-    '''mapProduc : mapProduct
-            | mapProudct COMMA map'''
+    '''mapProduc : mapArrow
+                 | mapArrow COMMA mapProduc'''
 
 def p_mapArrow(p):
     '''mapArrow : valor ARROWMAP valor'''
 #definir body
 def p_body(p):
-    '''body : '''
+    '''body : sentencia
+            | sentencia sentencias
+            | empty'''
 
 def p_imprimir(p):
     '''imprimir : LPAREN repiteValores RPAREN
@@ -131,6 +174,9 @@ def p_valor(t):
             | FLOAT
             | VARIABLE
             | STRING
+            | TRUE
+            | FALSE
+            | THIS
             | funcionParen'''
 
 def p_dataType(p):
@@ -153,18 +199,27 @@ def p_valorBool(p):
 def p_repiteValores(p):
     '''repiteValores : valor COMMA repiteValores
                 | valor
-                | empty'''
+                '''
 
 def p_operaArit(p):
     '''operaArit : valor
-                |  valor operador operaArit'''
-
+                |  valor operador operaArit
+                | valor DOT VALOR'''
+def p_expresion(p):
+    '''expresion : valor
+                 | operaArit
+                 | conditionProdu
+                 | ternario
+                 | accessMember'''
 def p_operador(p):
     '''operador :  PLUS
                 | MINUS
                 | TIMES
                 | DIVIDE
                 | MODULO'''
+def p_ternario(p):
+    '''ternario : conditionProdu QUESTION expresion COLON expresion'''
+                
 
 def p_empty(p):
     '''empty :'''
